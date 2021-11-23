665dfd2fd2881a28dc9ed4ac275357aba8478899
hquijano <herbert.quijano@gmail.com>
2021-11-23 01:30:38 -0600

    Assertions - in Checkbox java class

--- src/test/java/day1/UpdatedDropdown.java
+++ src/test/java/day1/UpdatedDropdown.java
@@ -3,6 +3,7 @@ package day1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+//TestNG is one of the testing frameworks that uses selenium
 
 public class UpdatedDropdown {
     public static void main(String[] args) throws InterruptedException {
