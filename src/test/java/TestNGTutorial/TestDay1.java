08d5f568e60ad45debcd13ac9dd6c99157b18a42
hquijano <herbert.quijano@gmail.com>
2021-12-22 00:05:42 -0600

    Selenium Grid, Selenium 4

---
+++ src/test/java/TestNGTutorial/TestDay1.java
@@ -0,0 +1,19 @@
+package TestNGTutorial;
+
+import org.testng.annotations.Test;
+
+public class TestDay1 {
+
+    @Test
+    public static void TestLogin() {
+        System.out.println("TestLogin");
+
+    }
+
+    @Test
+    public static void SecondTest() {
+        System.out.println("SecondTest");
+    }
+
+
+}
