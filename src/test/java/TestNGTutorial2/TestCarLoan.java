54b68494325489b0ec5c046c41b131df5ff4fda1
hquijano <herbert.quijano@gmail.com>
2021-12-25 15:19:22 -0600

    TestNG minicourse

--- src/test/java/TestNGTutorial2/TestCarLoan.java
+++ src/test/java/TestNGTutorial2/TestCarLoan.java
@@ -1,31 +1,46 @@
 package TestNGTutorial2;
 
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestCarLoan {
 
+    @AfterTest
+    public void LogoutCarLoan() {
+        System.out.println("Logging out of CarLoan");
+    }
+
     @BeforeTest
-    public void setUp()
-    {
+    public void setUp() {
         System.out.println("Setting up the test");
     }
 
+    @BeforeMethod
+    public void setUpMethod() {
+        System.out.println("Setting up Method");
+    }
+
     @Test
-    public void WebLoginCarLoan()
-    {
+    public void WebLoginCarLoan() {
         System.out.println("Web Login to CarLoan");
     }
 
-    @Test
-    public void MobileLoginCarLoan()
-    {
-        System.out.println("Mobile Login to CarLoan");
+    @BeforeSuite
+    public void setUpSuite() {
+        System.out.println("Setting up Suite");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        System.out.println("Tearing down Method");
     }
 
     @Test
-    public void RestAPILoginCarLoan()
-    {
+    public void RestAPILoginCarLoan() {
         System.out.println("Rest API Login to CarLoan");
+    }
+
+    @Test
+    public void MobileLoginCarLoan() {
+        System.out.println("(included) Mobile Login to CarLoan");
     }
 }
--- src/test/java/TestNGTutorial2/TestHomeLoan.java
+++ src/test/java/TestNGTutorial2/TestHomeLoan.java
@@ -1,35 +1,58 @@
 package TestNGTutorial2;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestHomeLoan {
     @Test
-    public void WebLoginHomeLoan()
-    {
-        System.out.println("Web Login to Home Loan");
+    public void WebLoginHomeLoan() {
+        System.out.println(" (5) Web Login to Home Loan");
     }
 
     @Test
-    public void MobileSignInHomeLoan()
-    {
-        System.out.println("Mobile Login to Home Loan");
+    public void MobileSignInHomeLoan() {
+        System.out.println(" (2) Mobile Sign In to Home Loan");
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        System.out.println("Tear down (Class)");
     }
 
     @Test
-    public void MobileViewActiveHomeLoan()
-    {
-        System.out.println("Mobile Login to Home Loan");
+    public void MobileViewActiveHomeLoan() {
+        System.out.println(" (3) Mobile View to Home Loan");
     }
 
     @Test
-    public void MobileLoginHomeLoan()
-    {
-        System.out.println("Mobile Login to Home Loan");
+    public void MobileLoginHomeLoan() {
+        System.out.println("(1) Mobile Login to Home Loan");
     }
 
     @Test
-    public void RestAPILoginHomeLoan()
-    {
-        System.out.println("Rest API Login to Home Loan");
+    public void RestAPILoginHomeLoan() {
+        System.out.println("(4) Rest API Login to Home Loan");
+    }
+
+    @AfterSuite
+    public void tearDownSuite() {
+        System.out.println("Tear down (Suite)");
+    }
+
+    @BeforeClass
+    public void setUp() {
+        System.out.println("Set up (Class)");
+    }
+
+    @Test
+    public void ToBeExcludedOne() {
+        System.out.println("To be excluded one");
+    }
+
+    @Test
+    public void ToBeExcludedTwo() {
+        System.out.println("To be excluded two");
     }
 }
